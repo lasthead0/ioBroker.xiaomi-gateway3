6951c527f71942e2902c4a65fba7fd8009836b82
Eugene Abramov <john.abramov@gmail.com>
2021-11-07 19:47:15 +0300

    Updated tests for parseXiaomiBle

--- test/unit.js
+++ test/unit.js
@@ -38,6 +38,16 @@ tests.unit(path.join(__dirname, '..'), {
                     .equals({'light': 1});
             });
 
+            it('eid == 0x1008 //4104', () => {
+                expect(Bluetooth.parseXiaomiBle({eid: 0x1008, edata: '63', pdid: 0})).to.deep
+                    .equals({'moisture': 99});
+            });
+
+            it('eid == 0x1009 //4105', () => {
+                expect(Bluetooth.parseXiaomiBle({eid: 0x1009, edata: 'fd08', pdid: 0})).to.deep
+                    .equals({'conductivity': 2301});
+            });
+
             it('eid == 0x100A //4106', () => {
                 expect(Bluetooth.parseXiaomiBle({eid: 0x100A, edata: '64', pdid: 1371})).to.deep
                     .equals({'battery': 100});
@@ -46,6 +56,36 @@ tests.unit(path.join(__dirname, '..'), {
             it('eid == 0x100D //4109', () => {
                 expect(Bluetooth.parseXiaomiBle({eid: 0x100D, edata: '27017601', pdid: 0})).to.deep
                     .equals({'temperature': 29.5, 'humidity': 37.4});
+            });
+
+            it('eid == 0x1010 //4112', () => {
+                expect(Bluetooth.parseXiaomiBle({eid: 0x1010, edata: '0900', pdid: 0})).to.deep
+                    .equals({'formaldehyde': 0.09});
+            });
+
+            it('eid == 0x1013 //4115', () => {
+                expect(Bluetooth.parseXiaomiBle({eid: 0x1013, edata: '63', pdid: 0})).to.deep
+                    .equals({'remaining': 99});
+            });
+
+            it('eid == 0x1014 //4116', () => {
+                expect(Bluetooth.parseXiaomiBle({eid: 0x1014, edata: '01', pdid: 0})).to.deep
+                    .equals({'water_leak': 1});
+            });
+
+            it('eid == 0x1015 //4117', () => {
+                expect(Bluetooth.parseXiaomiBle({eid: 0x1015, edata: '01', pdid: 0})).to.deep
+                    .equals({'smoke': 1});
+            });
+
+            it('eid == 0x1016 //4118', () => {
+                expect(Bluetooth.parseXiaomiBle({eid: 0x1016, edata: '01', pdid: 0})).to.deep
+                    .equals({'gas': 1});
+            });
+
+            it('eid == 0x1017 //4119', () => {
+                expect(Bluetooth.parseXiaomiBle({eid: 0x1017, edata: '00010000', pdid: 0})).to.deep
+                    .equals({'idle_time': 256});
             });
 
             it('eid == 0x1018 //4120', () => {
